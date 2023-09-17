75c92168dc9245026d793f046fe2ba1a5bb15291
FlavienMissier <Flavien.Missier@gmail.com>
2023-09-17 13:13:17 -0500

    Adds basic rust code to read JSON

--- translate/src/main.rs
+++ translate/src/main.rs
@@ -1,34 +1,53 @@
+use serde::{Serialize, Deserialize};
+use std::fs::File;
+use std::io::Read;
 use std::env;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Dictionary {
+    entries: Vec<Vocabulary>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "PascalCase")]
+struct Vocabulary {
+    spanish: String,
+    english: String,
+    french: String,
+    definition: String,
+    notes: String,
+    irregularities: String,
+    related: String,
+    category: String,
+    word_type: String,
+}
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "PascalCase")]
+struct Entry {
+    name: String,
+    definition: String,
+    keywords: Vec<String>,
+    related: String,
+    category: String,
+}
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let number = match args.get(1) {
-        Some(arg1) => { 
-            if match arg1.as_str().get(1) {
-               Some() =>,
-               None => 
-            } == "t" && arg1.len() == 3 {
-                match args.get(2 as usize) {
-                    Some(arg2) => Some(translate(&arg2, &arg2.get(0), &arg2.get(3))),
-                    None => {
-                        println!("Incorrect syntax");
-                        None
-                    },
-                }
-            }else{
-                println!("Invalid argument {}", arg1);
-                None
-            }
-        },
-        None => {
-            println!("Incorrect syntax");
-            None
-        },
-    };
-    match number {
-        Some(number) => println!("Translated: {}", number),
-        None => println!(),
+    let file = args.get(1).and_then(|arg1| File::open(arg1.trim()).ok());
+    let mut data = String::new();
+    match file {
+        Some(mut f) => {
+            f.read_to_string(&mut data).unwrap();
+        }
+        None => println!("incorrect argument")
     }
-}
-fn translate(keyword: &str, input_lang: &str, output_lang: &str) -> String{
-   String::from(keyword) 
+    let dict: Dictionary = serde_json::from_str(&data).expect("JSON was not well-formatted");
+    println!("{:?}", dict);
+    let dictionary = "/home/flavius/projects/scripts/dictionaries/spanish/vocabulary.json"; // get package to make ~ work and use it to make location more universal
+    // let dictionary = "vocabulary.json";
+    let mut file = File::open(dictionary).unwrap();
+    let mut data = String::new();
+    file.read_to_string(&mut data).unwrap();
+    // println!("{:?}", data);
+    let dict: Dictionary = serde_json::from_str(&data).expect("JSON was not well-formatted");
+    println!("{:?}", dict);
 }
